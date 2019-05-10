26840f1540c6beb686ed129f41ec6ec2ffd557ed
Patricia Loto <patricialoto@hotmail.com>
2019-05-10 13:41:47 -0300

    datos de latinoamerica

--- breveAnalisis.R
+++ breveAnalisis.R
@@ -74,11 +74,11 @@ mujeres <- ggplot(data =mundo, aes(x=(pais), y=totalMujeres, colour=pais, text =
 mujeres <- ggplot(data =mundo, aes(x=(pais), y=totalMujeres, colour=pais, text = paste('<b>País:</b>', pais,'\n <b> Mujeres:</b>', totalMujeres, '%' ))) + 
   geom_bar(size=1.5, stat="identity", position=position_dodge()) +
   scale_colour_manual(values =p) +  
-  #scale_fill_manual(values =c("turquoise", "maroon")) +
+  scale_fill_manual(values =p) +
   labs (x = "País", y = "Total", 
-        title= ("Mujeres en el Parlamento"))+
+        title= (""))+
   #caption = " Fuente: #DatosdeMiercoles", legend=" ")+ # agrego título al gráfico
-  theme (axis.text.x =element_blank(),
+  theme (axis.text.x =element_blank(),axis.ticks = ()
          #element_text(angle=90, vjust = 1, hjust=1.1, color="black", size=7),
          plot.title = element_text(family="Comic Sans MS",
                                    size=rel(1),                      #Tamaño relativo de la letra del título
@@ -93,10 +93,26 @@ mujeres <- ggplot(data =mundo, aes(x=(pais), y=totalMujeres, colour=pais, text =
          panel.background = element_blank(),
          panel.grid = element_blank(),
          rect = element_rect(fill = "pink", color = "pink"),
-         text = element_text(family = "Calibri", colour = "white", size = 14))
+         text = element_text(family = "Tahoma", colour = "white", size = 14))
 mujeres
 ggplotly(mujeres, hoverformat='2.F', tooltip = "text")
 #ggploty(mujeres)
+
+
+xaxis <- list(title = "",
+              showline = TRUE,
+              showgrid = FALSE,
+              showticklabels = TRUE,
+              linecolor = 'rgb(204, 204, 204)',
+              linewidth = 2,
+              autotick = FALSE,
+              ticks = 'outside',
+              tickcolor = 'rgb(204, 204, 204)',
+              tickwidth = 2,
+              ticklen = 5,
+              tickfont = list(family = 'Arial',
+                              size = 12,
+                              color = 'rgb(82, 82, 82)'))
 
 #-----------------------------------------------------------------------------------------
 # con PLOTLY con porcentaje total en todas las camaras
@@ -152,7 +168,7 @@ mujeres <- ggplot(data =mundo, aes(x=(pais), y=totalporcenM, colour=pais, text =
          panel.background = element_blank(),
          panel.grid = element_blank(),
          rect = element_rect(fill = "black", color = "black"),
-         text = element_text(family = "Roboto Condensed", colour = "white", size = 12))+
+         text = element_text(family = "Tahoma", colour = "white", size = 12))+
   
   # Here comes the gganimate specific bits
   transition_time(totalporcenM) +
@@ -167,14 +183,14 @@ ggploty(mujeres)
 #-----------------------------------------------------------------------------------------
 latam <- filter(datosParlamento, pais %in% c("Argentina", "Bolivia" , "Brasil","Chile", "Colombia", "Costa Rica", "Cuba", "Ecuador", "El Salvador", "Guatemala", "Honduras", "México", "Nicaragua", "Panamá", "Paraguay", "Puerto Rico", "Perú", "República Dominicana", "Uruguay", "Venezuela"))
 View(latam)
-latinoamerica<- datosParlamento%>% group_by(codPais, pais)%>% summarise(totalporcenM=(sum(porcentaje_mujeres,na.rm = TRUE)),totalMujeres=round((totalporcenM*10), 0), totalIntegrantes=(sum(nroIntegrantes, na.rm = TRUE)),totalporcenH=(100- totalporcenM))%>%arrange(desc(totalporcenM))
+latinoamerica<- latam%>% group_by(codPais, pais)%>% summarise(totalporcenM=(sum(porcentaje_mujeres,na.rm = TRUE)),totalMujeres=round((totalporcenM*10), 0), totalIntegrantes=(sum(nroIntegrantes, na.rm = TRUE)),totalporcenH=(100- totalporcenM))%>%filter(totalMujeres!=0)%>%arrange(desc(totalporcenM))
 View(latinoamerica)
 
 
-mujeres <- ggplot(data =mundo, aes(x=(pais), y=totalMujeres, colour=pais, text = paste('<b>País:</b>', pais,'\n <b> Mujeres:</b>', totalMujeres, '%' ))) + 
+mujeres2 <- ggplot(data =latinoamerica, aes(x=(pais), y=totalMujeres, colour=pais, text = paste('<b>País:</b>', pais,'\n <b> Mujeres:</b>', totalMujeres ))) + 
   geom_bar(size=1.5, stat="identity", position=position_dodge()) +
-  scale_colour_manual(values =p) +  
-  #scale_fill_manual(values =c("turquoise", "maroon")) +
+  scale_colour_manual(values =p1) +  
+  scale_fill_manual(values =c(values =p1)) +
   labs (x = "País", y = "Total", 
         title= ("Mujeres en el Parlamento"))+
   #caption = " Fuente: #DatosdeMiercoles", legend=" ")+ # agrego título al gráfico
@@ -193,9 +209,9 @@ mujeres <- ggplot(data =mundo, aes(x=(pais), y=totalMujeres, colour=pais, text =
          panel.background = element_blank(),
          panel.grid = element_blank(),
          rect = element_rect(fill = "pink", color = "pink"),
-         text = element_text(family = "Calibri", colour = "white", size = 14))
-mujeres
-ggplotly(mujeres, hoverformat='2.F', tooltip = "text")
+         text = element_text(family = "Tahoma", colour = "white", size = 14))
+mujeres2
+ggplotly(mujeres2, hoverformat='2.F', tooltip = "text")
 #ggploty(mujeres)
 
 
